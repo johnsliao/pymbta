8cfeb723b5ed5c0c6805e3e26a4388567c88462b
John Liao <js.liao08@gmail.com>
2016-01-27 21:30:45 -0500

    set up pypi package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'pymbta',
+  packages = ['pymbta'], # this must be the same as the name above
+  version = '1.0',
+  description = 'A random test lib',
+  author = 'John Liao',
+  author_email = 'john@johnliao.org',
+  url = 'https://github.com/johnsliao/pymbta', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['mbta', 'pymbta', 'wrapper'], # arbitrary keywords
+  classifiers = [],
+)
